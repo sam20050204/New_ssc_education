d936b9c420fdb09cfd4faea1e7b86185ce6f3ffd
sam20050204 <110318093+sam20050204@users.noreply.github.com>
2026-02-22 17:38:16 +0530

    Remove backup batch file from git tracking

--- backup_db.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Creating database backup...
-set timestamp=%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
-set timestamp=%timestamp: =0%
-copy db.sqlite3 "backups\db_backup_%timestamp%.sqlite3"
-echo Backup created: db_backup_%timestamp%.sqlite3
-pause
